a35f346d48facbaf51cab514818bea39644ce533
Fipaan <fipaan@proton.me>
2026-05-06 08:23:13 +0500

    slither: IERC20 -> SafeERC20
    Same as in TokenVesting

--- Dao-Project/src/treasury/Treasury.sol
+++ Dao-Project/src/treasury/Treasury.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Treasury {
+    using SafeERC20 for IERC20;
     address public immutable timelock;
 
     modifier onlyTimelock() {
@@ -38,9 +39,6 @@ contract Treasury {
         address to,
         uint256 amount
     ) external onlyTimelock {
-        require(
-            IERC20(token).transfer(to, amount),
-            "Token transfer failed"
-        );
+        IERC20(token).safeTransfer(to, amount);
     }
 }
